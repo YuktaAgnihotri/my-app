4bcaceb83fcf3ff2cb0e35f004b22d489526e776
YuktaAgnihotri <yuktaagnihotri8585@gmail.com>
2025-11-19 17:18:54 +0530

    fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import DashboardClient from "./DashboardClient";
 
 
 export default async function Dashboard() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/sales`, {
+  const res = await fetch(`/api/sales`, {
     cache: "no-store",
   });
  const data = await res.json();
