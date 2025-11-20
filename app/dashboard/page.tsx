fe67261acc725a53e94db2c4f8aaa648d0de1e64
YuktaAgnihotri <yuktaagnihotri8585@gmail.com>
2025-11-20 16:29:35 +0530

    serverSide rendering added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import DashboardClient from "./DashboardClient";
-
+export const dynamic = "force-dynamic"; 
+export const runtime = "edge";
 
 export default async function Dashboard() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/sales` , {
@@ -7,7 +8,6 @@ export default async function Dashboard() {
   });
  const data = await res.json();
 console.log("got data")
-console.log(data)
 
 
 // Filter when year changes
